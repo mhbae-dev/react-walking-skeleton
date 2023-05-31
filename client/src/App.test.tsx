ccf248ae16376ee3e5f25e1a3d1eacbe8786ecba
Myoung Bae <mhbae93@gmail.com>
2023-05-31 17:05:56 +0100

    created unit and e2e test for button

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { render,screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App", () => {
+  test("renders hello world on button click", async() => {
+    render(<App/>);
+
+    await act(async() => {
+      await userEvent.click(screen.getByRole("button"))
+    })
+
+    expect(screen.getByText("hello world")).toBeInTheDocument()
+  })
+})
---
+++ client/src/e2e/button.test.js
@@ -0,0 +1,18 @@
+import puppeteer from 'puppeteer'
+import "@testing-library/jest-dom";
+
+describe("App",() => {
+  it("render hello world after button click", async() => {
+    //render App
+    //find button use onClick
+    //expect hello world to be in document
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    await page.goto('http://localhost:3000')
+    await page.waitForSelector('button')
+    await page.click('button')
+    const document = await page.evaluate(() => document.body.textContent);
+    expect(document).toContain("hello world")
+  })
+})
