1f110a635585fd78a982916ba1d5b9c77c098272
Myoung Bae <mhbae93@gmail.com>
2023-06-02 12:21:21 +0100

    Updates

---
+++ precache-manifest.c5cc14a542a9f99d45e77023a7714f0c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "15909f80f28eabdd351e",
+    "url": "/react-walking-skeleton/static/js/main.a9b99173.chunk.js"
+  },
+  {
+    "revision": "0044571db7ff525dc92a",
+    "url": "/react-walking-skeleton/static/js/runtime~main.a45573f6.js"
+  },
+  {
+    "revision": "5bb0abb6add76cfe8723",
+    "url": "/react-walking-skeleton/static/js/2.8fec2e12.chunk.js"
+  },
+  {
+    "revision": "66a684e7f32da26b271bb94b567cd10c",
+    "url": "/react-walking-skeleton/index.html"
+  }
+];
